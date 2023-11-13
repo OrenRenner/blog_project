29dfc0dcf65437b1f2592e36368f38d3700d424f
Emil Nishanov <emil.nishanov@advncd.com>
2023-11-13 19:32:16 +0500

    Add functions on view + routing on urls, add templates and static files

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -3,28 +3,33 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', "published"
+        PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
-    )
-    status = models.CharField(
-        max_length=2, 
-        choices=Status.choices,
-        default=Status.DRAFT
-    )
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
