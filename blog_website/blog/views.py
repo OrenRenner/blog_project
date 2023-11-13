564692d96580bc72fd03019f3c7390fabfe5e0e2
Emil Nishanov <emil.nishanov@advncd.com>
2023-11-13 20:32:16 +0500

    Add get_absolute_url routing, add paginator

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             slug=post,
                              status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
