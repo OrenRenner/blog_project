9aafb168e061067a3201d3bedadcf013aa5a6095
Emil Nishanov <emil.nishanov@advncd.com>
2023-12-04 13:33:30 +0500

    Add features for website

--- blog_website/blog_website/settings.py
+++ blog_website/blog_website/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nishanovlilyemil@gmail.com'
-EMAIL_HOST_PASSWORD = 'mery vepq gmhk qzjw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
